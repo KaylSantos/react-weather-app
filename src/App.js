4c3e8a6f2da902642c3d770fb39946bc2b0c9bd5
KaylSantos <115588601+KaylSantos@users.noreply.github.com>
2023-02-16 09:48:51 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://sparkly-platypus-3811a9.netlify.app/" target="_blank">
-          Kayleigh Richardson
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/KaylSantos/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://sparkly-platypus-3811a9.netlify.app/"
+            target="_blank"
+          >
+            Kayleigh Richardson
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/KaylSantos/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
